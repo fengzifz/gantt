ed1764650e84b6a53682932caf7a7011c22e9908
Damon Chen <chenzifeng1987@gmail.com>
2018-08-06 09:26:48 +0800

    Localisation of months.

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -10,18 +10,18 @@ const SECOND = 'second';
 const MILLISECOND = 'millisecond';
 
 const month_names = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月'
 ];
 
 var date_utils = {
